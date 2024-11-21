f5b5f3669053c068ea7e070a7208547d6a8b1b22
Helio Leao <hmtleao@hotmail.com>
2024-11-21 11:57:03 -0300

    add activity indicator to accounts screen

--- app/(auth)/accounts.tsx
+++ app/(auth)/accounts.tsx
@@ -1,9 +1,10 @@
 import useAuthApi from "@/api/useAuthApi";
 import { useEffect, useState } from "react";
-import { View, Text } from "react-native";
+import { View, Text, ActivityIndicator } from "react-native";
 
 export default function Account() {
   const api = useAuthApi();
+  const [isLoading, setIsLoading] = useState(true);
   const [accounts, setAccounts] = useState([]);
 
   useEffect(() => {
@@ -11,12 +12,17 @@ export default function Account() {
       try {
         const response = await api("/accounts");
         setAccounts(response.data);
+        setIsLoading(false);
       } catch (error) {
         console.error(error);
       }
     };
     fetchAccounts();
   }, []);
+
+  if (isLoading) {
+    return <ActivityIndicator style={{ flex: 1 }} size={"large"} />;
+  }
 
   return (
     <View style={{ margin: 20 }}>
